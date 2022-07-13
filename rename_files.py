c66434141a8daf972ab814fb59bd6bf90dbc91b8
Mateus Ribeiro <75584091+devMateusRibeiro@users.noreply.github.com>
2022-07-12 21:28:35 -0300

    Add files via upload

---
+++ rename_files.py
@@ -0,0 +1,46 @@
+import os
+import time
+
+def validar_inputs_serie():
+    nome = input("Nome da série: ")
+    ano = input("Ano de lançamento: ")
+    temporada = input("Temporada: ")
+    formato = input("Formato dos arquivos: ")
+    
+    print("Revisando...")
+    print(f"{nome} ({ano}) - s{temporada}eXX.{formato}")
+    
+    print("Os valores passados estão corretos? S/N")
+    resposta = input(" ").upper()
+    
+    if("S" in resposta):
+        return nome, ano, temporada, formato    
+    
+    else:
+        print("Reiniciando a captura de dados sobre a série!")
+        validar_inputs_serie()
+
+def validar_inputs_arquivos():
+    caminho_origem = input("Pasta de origem: ")  
+    if(os.path.isdir(caminho_origem) != True):
+        print(f"O caminho {caminho_origem} não foi encontrado, insira novamente.")
+        validar_inputs_arquivos()
+        
+    caminho_destino = input("Pasta de destino: ")
+    if(os.path.isdir(caminho_destino) != True):
+        print(f"O caminho {caminho_destino} não foi encontrado, insira novamente.")
+        validar_inputs_arquivos()
+        
+    return caminho_origem, caminho_destino
+
+def rename_files():
+    print("BEM-VINDO")
+    
+    inputs_serie = validar_inputs_serie()
+    
+    inputs_arquivo = validar_inputs_arquivos()
+    
+    print("fim")
+    
+if __name__ == '__main__':
+    rename_files()
